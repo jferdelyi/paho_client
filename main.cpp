5999428e2b290bf9b5257679ab34f73569ffeef0
Jean-François Erdelyi <jean-francois.erdelyi@6mouv.com>
2022-11-10 16:25:58 +0100

    [QA] Add full example

--- main.cpp
+++ main.cpp
@@ -2,16 +2,17 @@
 #include <iostream>
 
 int main(int argc, char* argv[]) {
-	MQTTClient l_mqtt_client("JEAN-PATE");
+	MQTTClient l_mqtt_client("MQTTClient");
 	std::cout << l_mqtt_client.connect_to();
 
 	while (!l_mqtt_client.is_connected());
 
-	l_mqtt_client.subscribe_to("LOL");
+	l_mqtt_client.subscribe_to("TopicA");
+	l_mqtt_client.publish_to("TopicA", "Hello World");
 
 	while (std::tolower(std::cin.get()) != 'q');
 
-	l_mqtt_client.unsubscribe_to("LOL");
+	l_mqtt_client.unsubscribe_to("TopicA");
 	l_mqtt_client.disconnect_to_broker();
 	return EXIT_SUCCESS;
 }
